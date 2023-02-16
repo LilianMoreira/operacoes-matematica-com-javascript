5214a1296ceb01420295edf2c98a8da94ae3a8bb
Lilian Moreira <lilian_mpsantos@hotmail.com>
2023-02-16 17:05:34 -0300

    primeiro commit

---
+++ type_number.js
@@ -0,0 +1,21 @@
+// tipo number
+const meuNumero=3;
+const primeiroNumero=1;
+const segundoNumero=2;
+const operacaoMatematica= primeiroNumero + segundoNumero;
+console.log(operacaoMatematica)
+
+const subtracaoMatematica=primeiroNumero - segundoNumero;
+console.log(subtracaoMatematica)
+
+const multiplicaoMatematica=(primeiroNumero * segundoNumero);
+console.log(multiplicaoMatematica)
+
+const divisaoMatematica= (primeiroNumero/segundoNumero);
+console.log(divisaoMatematica)
+
+const meuPontoFlutuante=3.3;
+const meuPontoFlutuanteZero=.5;
+const novaOperacao= primeiroNumero/meuPontoFlutuante;
+console.log(novaOperacao)
+
