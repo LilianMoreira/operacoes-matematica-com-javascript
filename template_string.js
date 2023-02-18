4648481a8f3d1923323059ae0c8e30aba6ce9424
Lilian Moreira <lilian_mpsantos@hotmail.com>
2023-02-18 18:59:47 -0300

    evoluindo com as atividades

---
+++ template_string.js
@@ -0,0 +1,17 @@
+const  nome= "Ju";
+const idade= 2021-1981;
+const cidadeNascimento="São Paulo";
+
+//const apresentacao= "Meu nome é:" + " "+ nome + " " +  "tenho" + " "+ idade + " " + "nasci em"+ " "+ cidadeNascimento;
+const apresentacao=`Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`;
+console.log(apresentacao)
+
+const nome = "Leo";
+const idade = 23;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+const pedido = `${nome} diz: por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}`
+
+console.log(pedido)
+
+
