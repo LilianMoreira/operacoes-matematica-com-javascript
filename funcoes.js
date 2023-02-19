5dbf1593c1cd363cac88a1dcc1d146905d7fcfd5
Lilian Moreira <lilian_mpsantos@hotmail.com>
2023-02-18 21:26:34 -0300

    adicionando página de função

---
+++ funcoes.js
@@ -0,0 +1,23 @@
+//let x="";
+//console.log(x);
+//x="oi";
+//Declaração de funções
+//Declara a função
+function imprimeTexto(texto){
+console.log(texto)
+}
+//Executa a função
+imprimeTexto("oi");
+imprimeTexto("outro texto");
+
+function imprimeNumero (numero){
+    console.log(numero)
+}
+imprimeNumero("14");
+imprimeNumero("7");
+
+//três formas de escrever funções
+function soma(){
+    const resultado= 2 + 2;
+}
+console.log (soma ())
